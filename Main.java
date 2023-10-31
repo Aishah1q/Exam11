7ec3e7c7bbfacffb220e1efa043aa207fb8a3c57
Aishah1q <148686153+Aishah1q@users.noreply.github.com>
2023-10-31 11:09:26 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+      /*  System.out.println("please enter Number1");
+        int num1=input.nextInt();
+
+        if(num1%2==0){
+            System.out.println("The Number is Even");
+        }
+        else
+            System.out.println("Number is Odd");*/
+        //2
+      /*  int sum=0;
+        System.out.println("please enter a Number");
+        int num2=input.nextInt();
+        if(num2!=0)
+       for(int i=1;i<=num2;i++){
+               sum=sum+i;
+               System.out.println(sum);
+           }*/
+//3
+      /*  int[] Arr={10,-21,30,31,-25};
+        System.out.println(Arrays.toString(Arr));
+        for (int i :Arr){
+            if(i>0){
+                System.out.println("Postive number");
+            }
+            else System.out.println("Negative number");
+
+
+       }*/
+        //4
+      /*  String[] Arr1={"Tuwaiq","Bootcamp","Student","Java"};
+        for(String F:Arr1){
+            if(F.charAt(0)>F.charAt(1))
+             System.out.println(F);
+         }*/
+
+            }
+
+        }
+
+
